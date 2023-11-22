2de1ed1050b61f50d94784b999566d554eebe05e
Kardo Jankowitz <kardojv@gmail.com>
2023-11-22 11:44:12 +0200

    Updated authentication with md5 hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import mongoose from "mongoose";
-import encrypt from "mongoose-encryption";
+import md5 from "md5";
 
 const app = express();
 const port = 3000;
@@ -19,8 +19,6 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,7 +37,7 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     try {
         await newUser.save()
@@ -51,7 +49,7 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     try {
     const foundUser = await User.findOne({email: username});
     if (foundUser) {
