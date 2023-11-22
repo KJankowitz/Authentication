1b6bccef894fb82facfb3ec97e553424857b9d2a
Kardo Jankowitz <kardojv@gmail.com>
2023-11-22 10:35:50 +0200

    Add environment variables

---
+++ app.js
@@ -0,0 +1,69 @@
+import 'dotenv/config';
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+import mongoose from "mongoose";
+import encrypt from "mongoose-encryption";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb://127.0.0.1/userDB");
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+
+app.post("/register", async (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+    try {
+        await newUser.save()
+        res.render("secrets");
+    } catch (error) {
+        console.log(error.message);
+    }   
+});
+
+app.post("/login", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
+    const foundUser = await User.findOne({email: username});
+    if (foundUser) {
+        if (foundUser.password === password) {
+            res.render("secrets");
+        }
+    }
+    } catch (error) {
+        console.log(error.message);
+        }
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
