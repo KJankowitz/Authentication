7976b7e7b69550f883e232b8df6c9738cd5ab414
Kardo Jankowitz <kardojv@gmail.com>
2023-11-22 12:08:50 +0200

    bcrypt hashing with 10 saltrounds added

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import mongoose from "mongoose";
-import md5 from "md5";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
 
 const app = express();
 const port = 3000;
@@ -35,28 +37,36 @@ app.get("/register", (req, res) => {
 })
 
 app.post("/register", async (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-    try {
-        await newUser.save()
-        res.render("secrets");
-    } catch (error) {
-        console.log(error.message);
-    }   
+    
+     bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+
+        try {
+            await newUser.save()
+            res.render("secrets");
+        } catch (error) {
+            console.log(error.message);
+        }     
+    });
+    
 });
 
 app.post("/login", async (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     try {
     const foundUser = await User.findOne({email: username});
     if (foundUser) {
-        if (foundUser.password === password) {
-            res.render("secrets");
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            if (result === true) {
+                res.render("secrets");
+            }
+        });
+            
         }
-    }
     } catch (error) {
         console.log(error.message);
         }
